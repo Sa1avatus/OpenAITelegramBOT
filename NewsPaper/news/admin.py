2e3efa41de5028420fed7b7ad43932d1c489158d
Sa1avatus <singilarity@gmail.com>
2022-11-08 20:19:42 +0300

    2nd

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Author, Category, Post, PostCategory, Comment
 
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Author)
 # Register your models here.
