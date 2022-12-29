fd4dd0a9f7d38a466b5b98b2651f98d1372f589d
Salavatus <siingilarity@gmail.com>
2022-12-29 12:48:07 +0300

    2nd iteration

--- OpenAITelegramBOT/main.py
+++ OpenAITelegramBOT/main.py
@@ -3,7 +3,7 @@ import os
 
 import openai
 from telegram.ext import Filters
-from telegram.ext import MessageHandler
+from telegram.ext import MessageHandler, CallbackQueryHandler
 from telegram.ext import Updater
 import telegram
 from dotenv import load_dotenv
@@ -14,7 +14,9 @@ class DialogBot(object):
     def __init__(self, token, generator):
         self.updater = Updater(token=token)  # заводим апдейтера
         handler = MessageHandler(Filters.text | Filters.command, self.handle_message)
+        handler2 = CallbackQueryHandler(self.handle_callback)
         self.updater.dispatcher.add_handler(handler)  # ставим обработчик всех текстовых сообщений
+        self.updater.dispatcher.add_handler(handler2)
         self.handlers = collections.defaultdict(generator)  # заводим мапу "id чата -> генератор"
         self.model = None
 
@@ -24,16 +26,11 @@ class DialogBot(object):
     def handle_message(self, update, context):
         print("Received", update.message)
         chat_id = update.message.chat_id
-        if update.message.text == '/start':
+        if (update.message.text == '/start'):
             # если передана команда /start, начинаем всё с начала -- для
             # этого удаляем состояние текущего чатика, если оно есть
             self.handlers.pop(chat_id, None)
             answer = next(self.handlers[chat_id])
-        elif update.message.text.split('#')[0] == 'model':
-            print(f'model: {update.message.text}')
-            model = update.message.text
-            dialog_bot.model = model
-            answer = f'Вы выбрали модель {dialog_bot.model}'
         else:
             try:
                 print(f'try {self.model}')
@@ -51,6 +48,27 @@ class DialogBot(object):
                 return self.handle_message(update, context)
         print("Answer: %r" % answer)
         context.bot.sendMessage(chat_id=chat_id, text=answer, reply_markup=get_markup())
+
+    def handle_callback(self, update, context):
+        print("Received", update.callback_query)
+        chat_id = update.callback_query.message.chat_id
+        if (update.callback_query.data == '/start'):
+            # если передана команда /start, начинаем всё с начала -- для
+            # этого удаляем состояние текущего чатика, если оно есть
+            self.handlers.pop(update.message.chat_id, None)
+            answer = next(self.handlers[chat_id])
+        elif (update.callback_query.data.split('#')[0] == 'model'):
+            print(f'model: {update.callback_query.data}')
+            model = update.callback_query.data
+            dialog_bot.model = model
+            answer = f'Напишите, что вы хотите от модели:'
+        else:
+            del self.handlers[chat_id]
+            # (повторно вызванный, этот метод будет думать, что пользователь с нами впервые)
+            return self.handle_message(update, context)
+        print("Answer: %r" % answer)
+        context.bot.sendMessage(chat_id=chat_id, text=answer)
+        #context.bot.sendMessage(chat_id=chat_id, text=answer, reply_markup=get_markup())
 
     def dalle_model(self, text):
         response = openai.Image.create(
@@ -72,7 +90,7 @@ class DialogBot(object):
             frequency_penalty=0.0,
             presence_penalty=0.6,
             stop=[" Human:", " AI:"])
-        return f'```python\n{response["choices"][0]["text"]}\n```'
+        return f'```python\n{response["choices"][0]["text"]}\n\n\nИспользовалась модель: {response.model}```'
 
     def gpt3_model(self, text):
         print(f'TEXT: {text}')
@@ -86,25 +104,28 @@ class DialogBot(object):
             presence_penalty=0.6,
             stop=[" Human:", " AI:"])
         print(f'{response["choices"][0]["text"]}')
-        return f'{response["choices"][0]["text"]}'
+        return f'{response["choices"][0]["text"]}\n\nИспользовалась модель: {response.model}'
 
 
 def dialog():
     answer = yield "Выберите модель взаимодействия с ИИ:"
-    answer = yield "Напишите, что вы хотите от модели:"
+    #answer = yield "Напишите, что вы хотите от модели:"
     return answer
 
 def get_markup():
     item1 = telegram.InlineKeyboardButton(f'GPT-3', callback_data=f'model#davinchi')
     item2 = telegram.InlineKeyboardButton(f'DALL·E', callback_data=f'model#dalle')
     item3 = telegram.InlineKeyboardButton(f'Codex', callback_data=f'model#codex')
-    keys = [['model#davinchi','model#dalle','model#codex']]
-    keyboard = telegram.ReplyKeyboardMarkup(keys)
+    #keys = [['model#davinchi','model#dalle','model#codex']]
+    #keyboard = telegram.ReplyKeyboardMarkup(keys)
+    keyboard = telegram.InlineKeyboardMarkup([[item1, item2, item3]])
     #keyboard.add(item1, item2, item3)
     return keyboard
 
 
 if __name__ == "__main__":
+    start_sequence = "\nAI:"
+    restart_sequence = "\nHuman: "
     openai.api_key = os.getenv("OPENAI_API_KEY")
     completion = openai.Completion()
     token = os.getenv('BOT_TOKEN')
